ad65f0eac9e2e2a51d874c985d64262d7eed347b
yuudong123 <1mouri81@gmail.com>
2024-01-01 19:47:17 +0900

    comit

--- removeChzzkRanking.user.js
+++ removeChzzkRanking.user.js
@@ -11,19 +11,14 @@
 (function() {
     'use strict';
 
+    window.addEventListener('load', ()=>{
+        setTimeout(removeRankingDivafsdfsd, 1000)
+    })
+
     function removeRankingDivafsdfsd() {
         var targetElement = document.querySelector('div.live_chatting_ranking_container__RVHvS');
         if (targetElement) {
             targetElement.remove();
         }
     }
-
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-                removeRankingDivafsdfsd();
-            }
-        });
-    });
-    observer.observe(document.body, { childList: true, subtree: true });
 })();
