18d065088825262790f8b23a53061eaa0a1b12e6
yuudong123 <1mouri81@gmail.com>
2024-01-01 19:26:31 +0900

    commit

---
+++ 후원랭킹 지우기-0.1.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         후원랭킹 지우기
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  ㅈㄱㄴ
+// @author       역붕이
+// @match        https://chzzk.naver.com/live/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function removeRankingDivafsdfsd() {
+        var targetElement = document.querySelector('div.live_chatting_ranking_container__RVHvS');
+        if (targetElement) {
+            targetElement.remove();
+        }
+    }
+
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+                removeRankingDivafsdfsd();
+            }
+        });
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
